a14f34a517d9494754add74c3996ce320a516e99
Sammy Akharaz <ketrixz@gmail.com>
2018-03-27 15:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays"
+}
+function happyHolidaysto(name) {
+  return `Happy holidays, ${`
+}
+function happyHolidayto(holiday, name) {
+  return `Happy holidays`
+}
